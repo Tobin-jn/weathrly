83c131b5414087d319f7ec8ee7b0ac625698b8fa
Tobin Nelson <tobin.j.n@gmail.com>
2018-09-08 15:01:49 -0600

    Create weather conditions data set with icons

---
+++ src/conditions.js
@@ -0,0 +1,116 @@
+export const weatherConditions = {
+  'Clear': {
+    condition: 'Clear',
+    icon: './icons/clear.png'
+  },
+
+  'Partly Cloudy': {
+    condition: 'Partly Cloudy',
+    icon: './icons/partlycloudy.png'
+  },
+
+  'Mostly Cloudy': {
+    condition: 'Mostly Cloudy',
+    icon: './icons/partlycloudy.png'
+  },
+
+  'Cloudy': {
+    condition: 'Cloudy',
+    icon: './icons/cloudy.png'
+  },
+
+  'Hazy': {
+    condition: 'Hazy',
+    icon: './icons/foggy.png'
+  },
+
+  'Foggy': {
+    condition: 'Foggy',
+    icon: './icons/foggy.png'
+  },
+
+  'Very Hot': {
+    condition: 'Very Hot',
+    icon: './icons/hot.png'
+  },
+
+  'Very Cold': {
+    condition: 'Very Cold',
+    icon: './icons/cold.png'
+  },
+
+  'Blowing Snow': {
+    condition: 'Blowing Snow',
+    icon: './icons/snow.png'
+  },
+
+  'Chance of Showers': {
+    condition: 'Chance of Showers',
+    icon: './icons/snow.png'
+  },
+
+  'Showers': {
+    condition: 'Showers',
+    icon: './icons/rain.png'
+  },
+
+  'Chance of Rain': {
+    condition: 'Chance of Rain',
+    icon: './icons/rain.png'
+  },
+
+  'Rain': {
+    condition: 'Rain',
+    icon: './icons/rain.png'
+  },
+
+  'Chance of Thunderstorms': {
+    condition: 'Chance of Thunderstorms',
+    icon: './icons/thunderstorms.png'
+  },
+
+  'Thunderstorms': {
+    condition: 'Thunderstorms',
+    icon: './icons/thunderstorms.png'
+  },
+
+  'Flurries': {
+    condition: 'Flurries',
+    icon: './icons/snow.png'
+  },
+
+  'Chance of Snow Showers': {
+    condition: 'Chance of Snow Showers',
+    icon: './icons/snow.png'
+  },
+
+  'Snow Showers': {
+    condition: 'Snow Showers',
+    icon: './icons/snow.png'
+  },
+
+  'Chance of Snow': {
+    condition: 'Chance of Snow',
+    icon: './icons/snow.png'
+  },
+
+  'Snow': {
+    condition: 'Snow',
+    icon: './icons/snow.png'
+  },
+
+  'Chance of Ice Pellets': {
+    condition: 'Chance of Ice Pellets',
+    icon: './icons/thunderstorms.png'
+  },
+
+  'Ice Pellets': {
+    condition: 'Ice Pellets',
+    icon: './icons/thunderstorms.png'
+  },
+
+  'Blizzard': {
+    condition: 'Blizzard',
+    icon: './icons/snow.png'
+  },
+}
