fbccc38d7998c88d889e90d25f8e80ef90770fa2
Tobin Nelson <tobin.j.n@gmail.com>
2018-09-12 16:40:01 -0600

    Test errors commit

--- src/App-test.js
+++ src/App-test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 import App from './App';
-// import 'boilerplate';
 
 describe('App', () => {
   
-  it('should exist', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper).toBeDefined()
-  })
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  }):
 
 })
